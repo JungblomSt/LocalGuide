18f5a0ba59259ef54d66b37b0b40f5184a556d56
Stina Thun <3kdapp25.thunst@folkuniversitetet.nu>
2026-05-12 16:20:52 +0200

    Initial commit

---
+++ LocalGuide/LocalGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalGuideApp.swift
+//  LocalGuide
+//
+//  Created by Stina Thun on 2026-05-12.
+//
+
+import SwiftUI
+
+@main
+struct LocalGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
