04f14cd29511de7bb65322d7aafc7d8aa3844ce7
Shallange <jimmy.kroneld@hotmail.com>
2026-05-16 20:59:51 +0200

    Set initial homepage map region

--- LocalGuide/Views/HomeMapView.swift
+++ LocalGuide/Views/HomeMapView.swift
@@ -8,10 +8,25 @@
 import SwiftUI
 import MapKit
 
-
 struct HomeMapView: View {
+    // Initial map region centered over Sweden
+    // Latitude and longitude set the center point of the map
+    // Delta values control the zoom level: higher values show a larger area
+    @State private var cameraPosition: MapCameraPosition = .region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(
+                latitude: 59.0,
+                longitude: 16.0
+            ),
+            span: MKCoordinateSpan(
+                latitudeDelta: 9,
+                longitudeDelta: 9
+            )
+        )
+    )
+
     var body: some View {
-        Map()
+        Map(position: $cameraPosition)
             .mapStyle(.hybrid)
     }
 }
