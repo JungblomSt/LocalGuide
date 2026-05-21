5a04ec81e0ff5843bf7e143c1998e66e255103c8
Nedakhalaj <neda.khalaj7168@gmail.com>
2026-05-21 11:17:45 +0200

    Added the Firestore implementation

--- LocalGuide/LocalGuideApp.swift
+++ LocalGuide/LocalGuideApp.swift
@@ -6,14 +6,18 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct LocalGuideApp: App {
-  
+
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
-                
         }
     }
 }
