2fef766a235eff457e475ecf1995f8d124d8a7dd
GreedyGobling <greedygobling@gmail.com>
2026-05-26 11:30:32 +0200

    changed to avfoundation
    found more doc/info about AVfoundation

--- LocalGuide/Viewmodels/AudioPlayerManager.swift
+++ LocalGuide/Viewmodels/AudioPlayerManager.swift
@@ -5,7 +5,25 @@
 //  Created by robin on 2026-05-25.
 //
 
-import AVFAudio
+import AVFoundation
 
+class AudioPlayerManager: ObservableObject {
+    private var player: AVAudioPlayer?
 
-// class AVAudioPlayer
+    @Published var isPlaying = false
+
+    func play(url: URL) {
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            isPlaying = true
+        } catch {
+            print("AudioPlayerManager: failed to play \(url): \(error)")
+        }
+    }
+
+    func stop() {
+        player?.stop()
+        isPlaying = false
+    }
+}
