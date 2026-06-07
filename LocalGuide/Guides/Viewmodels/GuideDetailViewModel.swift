39e0366cecf08b73ac9aabcd08d5fc8f465b3f28
GreedyGobling <greedygobling@gmail.com>
2026-06-07 04:48:27 +0200

    it works.

--- LocalGuide/Guides/Viewmodels/GuideDetailViewModel.swift
+++ LocalGuide/Guides/Viewmodels/GuideDetailViewModel.swift
@@ -5,6 +5,9 @@ class GuideDetailViewModel {
     private let audioPlayer = AudioPlayerManager()
     let guide: Guide
 
+    /// True medan ljudet laddas ner från Firebase Storage första gången
+    var isLoadingAudio = false
+
     var isPlaying: Bool { audioPlayer.isPlaying }
     var isAudioLoaded: Bool { audioPlayer.isLoaded }
     var currentTime: TimeInterval { audioPlayer.currentTime }
@@ -20,10 +23,18 @@ class GuideDetailViewModel {
             audioPlayer.pause()
         } else if audioPlayer.isLoaded {
             audioPlayer.resume()
-        } else if let name = guide.audioURL,
-                  let url = Bundle.main.url(forResource: name, withExtension: nil) {
-            audioPlayer.play(url: url)
+        } else if let path = guide.audioURL {
+            // Hämtar ljudet från Storage (med disk-cache) och spelar upp det
+            Task { await loadAndPlay(path: path) }
         }
+    }
+
+    @MainActor
+    private func loadAndPlay(path: String) async {
+        guard !isLoadingAudio else { return }
+        isLoadingAudio = true
+        await audioPlayer.play(storagePath: path)
+        isLoadingAudio = false
     }
 
     func seek(to time: TimeInterval) {
